f11e66dde0011d138ba9562d0f4e36e1a14578e0
jave <111729472+mokkajave@users.noreply.github.com>
2025-09-26 13:36:45 +0200

    Artikkel 1: Lagt inn script og bilder
    - Sladd over øynene ved scroll (ikke ferdig) - kun konsept
    - Ryddet opp i CSS struktur
    - Lagt inn bilder

--- sak1/js/script.js
+++ sak1/js/script.js
@@ -8,3 +8,15 @@ slider.addEventListener("input", function () {
     const opacityValue = this.value / 100;
     overlay.style.opacity = opacityValue;
 });
+
+window.addEventListener("scroll", function () {
+    const scrollTrigger = 1550;
+    const censor = document.getElementById("eye-censor");
+
+    if (window.scrollY > scrollTrigger) {
+        censor.classList.add("visible");
+    }
+    else {
+        censor.classList.remove("visible");
+    }
+});
