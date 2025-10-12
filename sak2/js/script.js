64e05665609e122e800ee886038dfac4ee011f6f
jave <111729472+mokkajave@users.noreply.github.com>
2025-10-13 01:28:48 +0200

    Bedre animasjoner

--- sak2/js/script.js
+++ sak2/js/script.js
@@ -1,8 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
     const observerOptions = {
-        threshold: 0.1
+        threshold: 0.3
     };
 
+    // Fade up-seksjon
     const fadeUpElements = document.querySelectorAll(".fade-up");
 
     const observer = new IntersectionObserver((entries, observer) => {
@@ -15,4 +16,32 @@ document.addEventListener("DOMContentLoaded", () => {
     }, observerOptions);
 
     fadeUpElements.forEach(el => observer.observe(el));
+
+
+    // Swiper-seksjoner
+    const swipeObserver = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                const section = entry.target;
+
+                section.classList.add("swipe-in");
+
+                setTimeout(() => {
+                    const textContainer = section.querySelector(".text-container");
+
+                    if (textContainer) {
+                        textContainer.querySelectorAll(".fade-up-swipe").forEach(el => {
+                            el.classList.add("visible");
+                        });
+                    }
+                }, 500)
+
+                observer.unobserve(section);
+            }
+        });
+    }, observerOptions);
+
+    document.querySelectorAll(".color-swipe-section").forEach(section => {
+        swipeObserver.observe(section);
+    });
 });
