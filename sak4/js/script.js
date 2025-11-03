174129cc103f831433c83db0cbf4065ee9238896
jave <111729472+mokkajave@users.noreply.github.com>
2025-11-03 22:05:28 +0100

    Design, animasjon og layout
    Gjort endringer i sak 2 og 4: Fjernet snap på seksjoner i sak 4; Lagt til parallax-effekt på scroll i sak 4; Endret generelle størrelsesforhold

---
+++ sak4/js/script.js
@@ -0,0 +1,80 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const indicator = document.querySelector(".scroll-indicator");
+    const hero = document.querySelector(".hero-section");
+    const heroContent = document.querySelector(".hero-content");
+    const elementsToReveal = document.querySelectorAll(".hide-until-ready");
+
+    const HERO_FACTOR = 3;
+    const CONTENT_FACTOR = 10;
+
+    let latestScrollY = 0;
+    let ticking = false;
+
+    function updateParallax(scrollPos) {
+        // Hero-bakgrunn
+        const heroYOffset = scrollPos / HERO_FACTOR;
+        hero.style.transform = `translate3d(0, ${-heroYOffset}px, 0)`;
+
+        // Hero tekst
+        const contentYOffset = scrollPos / CONTENT_FACTOR;
+        heroContent.style.transform = `translate3d(0, ${-contentYOffset}px, 0)`;
+
+        ticking = false;
+    }
+
+    function handleScroll() {
+        latestScrollY = window.scrollY;
+        if (!ticking) {
+            window.requestAnimationFrame(() => updateParallax(latestScrollY));
+            ticking = true;
+        }
+    }
+
+    window.addEventListener("scroll", handleScroll, { 
+        passive: true 
+    });
+
+    setTimeout (() => {
+        handleScroll();
+
+        if (elementsToReveal.length > 0) {
+            elementsToReveal.forEach(el => el.classList.remove("hide-until-ready"));
+        }
+    }, 50);
+
+    // --- Animasjon --- /
+ 
+    const observer = new IntersectionObserver(
+        (entries) => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    indicator.classList.remove("hidden");
+                } else {
+                    indicator.classList.add("hidden");
+                }
+            });
+        },
+        {
+            threshold: 1
+        }
+    );
+    observer.observe(hero);
+
+    const sections = document.querySelectorAll(".fade-up");
+
+    const fadeObserver = new IntersectionObserver(
+        (entries, observer) => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    entry.target.classList.add("visible");
+                    observer.unobserve(entry.target);
+                }
+            })
+        },
+        {
+            threshold: 0.2
+        }
+    );
+    
+    sections.forEach(section => fadeObserver.observe(section));
+});
